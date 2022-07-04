307ca5fad58d0ddd39c160141e792e8422070a5d
exomat <exomat@wp.pl>
2022-07-04 21:10:36 +0200

    init spring project

---
+++ recruitment-task-backend/src/main/java/com/mpernal/recruitmenttaskbackend/RecruitmentTaskBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mpernal.recruitmenttaskbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentTaskBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentTaskBackendApplication.class, args);
+	}
+
+}
---
+++ recruitment-task-backend/src/test/java/com/mpernal/recruitmenttaskbackend/RecruitmentTaskBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mpernal.recruitmenttaskbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecruitmentTaskBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
